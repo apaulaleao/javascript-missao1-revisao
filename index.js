7af78487c268e34a52667276f59ea0f421a13ea2
Ana Paula Leão <83734454+apaulaleao@users.noreply.github.com>
2024-08-14 01:08:06 +0000

    update

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello world!!!!");
+//console.log("Hello world !!!!");
+
+const carro ={
+    marca : 'toyota',
+    modelo: 'corola',
+    ligar : function () {
+        console.log(`O ${this.marca} está ligado`);     
+        
+    }
+}
+carro.ligar()
+// console.log(carro);
+
+    
+
+// desligar: function () {
+//     console.log("o carro esta desligado");
+// for ( let i=0; i< 15; i++){
+//     console.log(i);
+    
+//}
+const frutas = ["maca","banana", "laranja"]
+// console.log(frutas);
+
+for (const key in frutas) {
+    console.log(frutas, frutas[key]);
+    
+        
+    }
